68b4119ebd0c71b00c65471dd31afbac67131d5f
timeurg <timeurg@gmail.com>
2024-04-19 17:47:33 +0000

    feat: dockerization

--- read-n-write/apps/reader/src/main.ts
+++ read-n-write/apps/reader/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ReaderModule } from './reader.module';
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(ReaderModule);
+  app.enableShutdownHooks();
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
